feeb95df1853250d4b8dbb886b4db1670a429dcc
Vikram Singh <vikram0957@gmail.com>
2025-12-06 08:43:17 +0530

    added some question of stream api

--- hdfc/src/main/java/com/HdfcApplication.java
+++ hdfc/src/main/java/com/HdfcApplication.java
@@ -1,4 +1,4 @@
-package com.hdfc;
+package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ hdfc/src/main/java/com/streamapi/md/SumOfAllElements.java
@@ -0,0 +1,12 @@
+package com.streamapi.md;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class SumOfAllElements {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+        int sum = numbers.stream().mapToInt(Integer::intValue).sum();
+        System.out.println(sum);
+    }
+}
