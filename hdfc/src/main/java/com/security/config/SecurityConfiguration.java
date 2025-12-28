c2523f24ab7e5d91190af80e00717ff02a605f6f
Vikram Singh <vikram0957@gmail.com>
2025-12-28 20:23:12 +0530

    this is working code

--- hdfc/src/main/java/com/security/config/SecurityConfiguration.java
+++ hdfc/src/main/java/com/security/config/SecurityConfiguration.java
@@ -30,7 +30,7 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(AbstractHttpConfigurer::disable)
+                .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers("/actuator/**").permitAll()
